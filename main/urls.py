2e5ca3fcefa95cc42ee32dae2c8911658a892615
debora <debora.barros@ccc.ufcg.edu.br>
2022-08-02 20:10:25 -0300

    add client

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 # router.register('url..../', views.Modelo, basename='algumacoisa')
 
 router.register(r'users', views.UserViewSet, basename="Users")
+router.register(r'clients', views.ClientViewSet, basename="Clients")
 router.register(r'projects', views.ProjectViewSet, basename="Projects")
 
 
