4d13badd1d41b7b57a6e98cca8617b0fadeab718
Débora Barros <34282455+deborafb@users.noreply.github.com>
2022-08-16 21:00:46 -0300

    Update settings.py

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -162,7 +162,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {     
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.pagination.PageNumberPagination',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',     
     )    
