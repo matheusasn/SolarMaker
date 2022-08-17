92c1bbd7c1ee5f464622a7ab72e9fc940f085603
isaiasmtp <isaiasmteixeira@gmail.com>
2022-08-17 10:07:50 -0300

    Fix auth token

--- solarmaker/app/main/views.py
+++ solarmaker/app/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
+
 from . import models, serializers
 
 class Teste(APIView):
@@ -13,20 +15,20 @@ class Teste(APIView):
 class ClientViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ClientSerializer
     queryset = models.Client.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = (IsAuthenticated,)
 
 class ClientDocumentsViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ClientDocumentSerializer
     queryset = models.ClientDocument.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = (IsAuthenticated,)
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ProjectSerializer
     queryset = models.Project.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = (IsAuthenticated,)
 
 class ProjectDocumentsViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ProjectDocumentSerializer
     queryset = models.ProjectDocument.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = (IsAuthenticated,)
 
