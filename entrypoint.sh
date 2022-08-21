2c24cfedb1be5b3ceff3e37fd451f8671b245d5d
Débora Barros <34282455+deborafb@users.noreply.github.com>
2022-08-21 10:32:58 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ python manage.py flush --no-input
 python manage.py migrate
 
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 
 
 exec "$@"
