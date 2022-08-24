57f110c3b49cbb45b0458db6e42b054b2d63eebc
kamilaalbuquerquecc <kamila.albuquerque@ccc.ufcg.edu.br>
2022-08-23 22:15:29 -0300

    testes para post e get

--- main/tests/integration_tests/test_get.py
+++ main/tests/integration_tests/test_get.py
@@ -1,7 +1,7 @@
 import requests
 
 class TestGetClientsSemAutorizacao:
-    headers ={'Authorization': 'Token 658a0fa9b5e469a22792558a413e8f9705b7a414'} 
+    
     url_base_clients = 'http://localhost:8000/api/v1/clientes/'
 
     def test_get_client_sem_autorizacao_por_token(self):
@@ -21,6 +21,28 @@ class TestGetProjectsSemAutorizacao:
         assert resultado.json()['detail']== 'As credenciais de autenticação não foram fornecidas.'
         assert resultado.status_code == 401
 
+class TestGetClientsTokenIvalido:
+
+    headers ={'Authorization': 'Token 658a0fa9b5e469a22792558a413e8f9705b7a415'}
+    url_base_clients = 'http://localhost:8000/api/v1/clientes/'
+
+    def test_get_client_token_ivalido(self):
+
+        resultado= requests.get(url=self.url_base_clients, headers = self.headers)
+        assert resultado.json()['detail']== 'Token inválido.'
+        assert resultado.status_code == 401
+
+class TestGetProjectsTokenIvalido:
+
+    headers ={'Authorization': 'Token 658a0fa9b5e469a22792558a413e8f9705b7a415'}
+    url_base_projects = 'http://localhost:8000/api/v1/projetos/'
+
+    def test_get_project_token_ivallido(self):
+        
+        resultado = requests.get(url=self.url_base_projects, headers = self.headers)
+        assert resultado.json()['detail']== 'Token inválido.'
+        assert resultado.status_code == 401
+
 class TestGetClients:
   
     headers ={'Authorization': 'Token 658a0fa9b5e469a22792558a413e8f9705b7a414'} 
@@ -35,7 +57,7 @@ class TestGetClients:
 
 class TestGetProjets:
 
-    headers ={'Authorization': 'Token 658a0fa9b5e469a22792558a413e8f9705b7a414'}  
+    headers ={'Authorization': 'Token 658a0fa9b5e469a22792558a413e8f9705b7a414'} 
 
     url_base_projects = 'http://localhost:8000/api/v1/projetos/'
 
