18ecbab0b16268a97a3b1045c251053d39dbbf02
Débora Barros <34282455+deborafb@users.noreply.github.com>
2022-08-27 21:05:03 -0300

    Update serializers.py

--- main/serializers.py
+++ main/serializers.py
@@ -10,7 +10,7 @@ class UserManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         model.is_staff = True
-        fields = ['id', 'username', 'email', 'password', 'is_superuser']
+        fields = ['id', 'username', 'email', 'password', 'is_staff']
     
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data.get('password'))
