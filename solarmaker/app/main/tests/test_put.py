c87abc9bd97e3c95cb40822633b646bf29a6de17
kamilaalbuquerquecc <kamila.albuquerque@ccc.ufcg.edu.br>
2022-08-28 00:22:12 -0300

    tests de adicionar dados ok

--- solarmaker/app/main/tests/test_put.py
+++ solarmaker/app/main/tests/test_put.py
@@ -2,7 +2,7 @@ import requests
 
 
 class TestPutClients:
-    headers ={'Authorization': 'Token 658a0fa9b5e469a22792558a413e8f9705b7a414'} 
+    headers ={'Authorization': 'Token 7788d06479c048fd17dbacc1d2d6e46628650621'} 
     url_base_clients = 'http://localhost:8000/api/v1/clientes/'
 
     clients = requests.get(url=url_base_clients, headers = headers)
@@ -12,12 +12,12 @@ class TestPutClients:
     new_client = {
         "name": "Kamila Silva",
         "email": "kamilas@gmail.com",
-        "phone_number": "(83)99158-0860",
+        "phone_number": "(83)99158-0845",
         "adress":"Rua 5",
         "cpf_cnpj": "70352072450"
     }
     def test_put_client(self):
         
-        resultado = requests.put(url=f'{self.url_base_clients}{self.cpf_cnpj}/', headers = self.headers, data=self.new_client)
+        resultado = requests.put(url=f'{self.url_base_clients}70352072450/', headers = self.headers, data=self.new_client)
         assert resultado.status_code == 200
 
